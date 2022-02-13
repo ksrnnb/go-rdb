39dcd7c4a7eb6a511ffa1c01c688d3148a9c3418
ksrnnb <enrico.fermi.phys@gmail.com>
2022-02-13 09:35:11 +0900

    fix log package name

---
+++ logs/log_iterator_test.go
@@ -0,0 +1,47 @@
+package logs
+
+import (
+	"testing"
+
+	"github.com/ksrnnb/go-rdb/file"
+)
+
+const dbDirectory = "./../data"
+
+func newLogIterator(t *testing.T) *LogIterator {
+	t.Helper()
+	fm := newFileManaer(t)
+
+	blk := file.NewBlockID("tempBlock", 0)
+	li, err := NewLogIterator(fm, blk)
+
+	if err != nil {
+		t.Fatalf("failed to create new log iterator, %v", err)
+	}
+
+	return li
+}
+
+func newFileManaer(t *testing.T) *file.FileManager {
+	t.Helper()
+	fm, err := file.NewFileManager(dbDirectory, 400)
+
+	if err != nil {
+		t.Fatalf("failed to create file manager, %v", err)
+	}
+	return fm
+}
+
+func TestIteratesLogIterator(t *testing.T) {
+	li := newLogIterator(t)
+
+	if !li.HasNext() {
+		t.Fatalf("HasNext() should be true but get false")
+	}
+
+	_, err := li.Next()
+
+	if err != nil {
+		t.Errorf("Next() failed, %v", err)
+	}
+}
