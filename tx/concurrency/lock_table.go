410193377f562330e4e5ca4112233b860a0e3eee
ksrnnb <enrico.fermi.phys@gmail.com>
2022-02-27 09:22:12 +0900

    lock table

---
+++ tx/concurrency/lock_table.go
@@ -0,0 +1,115 @@
+package concurrency
+
+import (
+	"errors"
+	"time"
+
+	"github.com/ksrnnb/go-rdb/file"
+)
+
+const maxTimeMilliSecond = 10000
+const xLocked = -1
+
+var ErrLockAbort = errors.New("concurrency: lock abort error")
+
+type Lock struct {
+	blk *file.BlockID
+	val int // -1 is xLocked, over 1 is sLocked, 0 is unlocked
+}
+
+type LockTable struct {
+	locks []*Lock
+}
+
+func NewLockTable() *LockTable {
+	return &LockTable{}
+}
+
+// todo: sync
+func (lt *LockTable) SLock(blk *file.BlockID) error {
+	start := time.Now()
+	for lt.hasXLock(blk) && !isWaitingTooLong(start) {
+		// TODO: 修正
+		time.Sleep(1 * time.Second)
+	}
+
+	if lt.hasXLock(blk) {
+		return ErrLockAbort
+	}
+
+	val := lt.getLockVal(blk)
+	lt.setLockVal(blk, val+1)
+
+	return nil
+}
+
+func (lt *LockTable) hasXLock(blk *file.BlockID) bool {
+	return lt.getLockVal(blk) == xLocked
+}
+
+func (lt *LockTable) XLock(blk *file.BlockID) error {
+	start := time.Now()
+	for lt.hasOtherSLocks(blk) && !isWaitingTooLong(start) {
+		// TODO: 修正
+		time.Sleep(1 * time.Second)
+	}
+
+	if lt.hasOtherSLocks(blk) {
+		return ErrLockAbort
+	}
+
+	lt.setLockVal(blk, xLocked)
+	return nil
+}
+
+func (lt *LockTable) hasOtherSLocks(blk *file.BlockID) bool {
+	// TODO: 要確認
+	// 書籍では > 1
+	return lt.getLockVal(blk) >= 1
+}
+
+func (lt *LockTable) Unlock(blk *file.BlockID) {
+	val := lt.getLockVal(blk)
+
+	if val > 1 {
+		lt.setLockVal(blk, val-1)
+	} else {
+		lt.deleteLock(blk)
+	}
+}
+
+func (lt *LockTable) getLockVal(blk *file.BlockID) int {
+	for _, lock := range lt.locks {
+		if blk.Equals(lock.blk) {
+			return lock.val
+		}
+	}
+
+	return 0
+}
+
+func (lt *LockTable) setLockVal(blk *file.BlockID, val int) {
+	for _, lock := range lt.locks {
+		if blk.Equals(lock.blk) {
+			lock.val = val
+		}
+	}
+}
+
+// deleteLock() delete specified block
+func (lt *LockTable) deleteLock(blk *file.BlockID) {
+	for key, lock := range lt.locks {
+		if blk.Equals(lock.blk) {
+			// https://github.com/golang/go/wiki/SliceTricks#delete
+			copy(lt.locks[key:], lt.locks[key+1:])
+			lt.locks[len(lt.locks)-1] = nil
+			lt.locks = lt.locks[:len(lt.locks)-1]
+		}
+	}
+}
+
+func isWaitingTooLong(start time.Time) bool {
+	limit := start.Add(maxTimeMilliSecond * time.Millisecond)
+
+	return time.Now().After(limit)
+}
