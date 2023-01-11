aa72c23fcfea9cbeeb5529fa60b353c711e7808f
K. Okabe <48155865+ksrnnb@users.noreply.github.com>
2023-01-11 19:06:59 +0900

    :green_heart: add pin test

--- buffer/buffer_manager_test.go
+++ buffer/buffer_manager_test.go
@@ -2,6 +2,7 @@ package buffer_test
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 
 	. "github.com/ksrnnb/go-rdb/buffer"
@@ -37,15 +38,35 @@ func TestPin(t *testing.T) {
 	require.NoError(t, err)
 	buffers[2], err = bm.Pin(blockID(t, filename, 2))
 	require.NoError(t, err)
+	assert.Equal(t, 0, bm.Available())
 
 	bm.Unpin(buffers[1])
 	buffers[1] = nil
+	assert.Equal(t, 1, bm.Available())
 
-	// 0番はpinされたままなので、numAvailableは増えない
+	// 0番はpinされたままなので、numAvailableは変化しない
 	buffers[3], err = bm.Pin(blockID(t, filename, 0))
 	require.NoError(t, err)
+	assert.Equal(t, 1, bm.Available())
+
 	buffers[4], err = bm.Pin(blockID(t, filename, 1))
 	require.NoError(t, err)
+	assert.Equal(t, 0, bm.Available())
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		buffers[4], err = bm.Pin(blockID(t, filename, 3))
+		require.NoError(t, err)
+		wg.Done()
+	}()
+
+	// unpin することで空きができて、blockID 3 の pin が処理を再開する
+	bm.Unpin(buffers[4])
+	buffers[4] = nil
+	wg.Wait()
+
+	assert.Equal(t, 0, bm.Available())
 
 	for i := 0; i < len(buffers); i++ {
 		b := buffers[i]
