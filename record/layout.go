e1bccd43dce1a3a49237e4480bfdaa4d6e3b2288
K. Okabe <48155865+ksrnnb@users.noreply.github.com>
2023-01-17 20:56:25 +0900

    add layout/schema

---
+++ record/layout.go
@@ -0,0 +1,40 @@
+package record
+
+import "fmt"
+
+type Layout struct {
+	schema   *Schema
+	offsets  map[string]int
+	slotSize int
+}
+
+func NewLayout(s *Schema) (*Layout, error) {
+	offsets := make(map[string]int)
+	pos := IntByteSize
+	for _, fn := range s.Fields() {
+		offsets[fn] = pos
+		ofs, err := s.lengthInBytes(fn)
+		if err != nil {
+			return nil, err
+		}
+		pos += ofs
+	}
+	return &Layout{s, offsets, pos}, nil
+}
+
+func (l *Layout) Schema() *Schema {
+	return l.schema
+}
+
+func (l *Layout) Offset(fieldName string) (int, error) {
+	for fn, ofs := range l.offsets {
+		if fn == fieldName {
+			return ofs, nil
+		}
+	}
+	return 0, fmt.Errorf("invalid field name [%s]", fieldName)
+}
+
+func (l *Layout) SlotSize() int {
+	return l.slotSize
+}
---
+++ record/schema.go
@@ -0,0 +1,113 @@
+package record
+
+import (
+	"fmt"
+
+	"github.com/ksrnnb/go-rdb/file"
+)
+
+const IntByteSize = 4
+
+type FieldType uint8
+
+const (
+	Unknown FieldType = iota
+	Integer
+	String
+)
+
+type FieldInfo struct {
+	fieldType FieldType
+	length    int
+}
+
+func NewFieldInfo(fieldType FieldType, length int) *FieldInfo {
+	return &FieldInfo{fieldType,
+		length}
+}
+
+type Schema struct {
+	fields    []string
+	fieldInfo map[string]*FieldInfo
+}
+
+func NewSchema() *Schema {
+	return &Schema{}
+}
+
+func (s *Schema) AddIntField(fieldName string) {
+	s.addField(fieldName, Integer, 0)
+}
+
+func (s *Schema) AddStringField(fieldName string, length int) {
+	s.addField(fieldName, String, length)
+}
+
+func (s *Schema) Add(fieldName string, ss *Schema) error {
+	ft, err := ss.fieldType(fieldName)
+	if err != nil {
+		return err
+	}
+
+	l, err := ss.length(fieldName)
+	if err != nil {
+		return err
+	}
+
+	s.addField(fieldName, ft, l)
+	return nil
+}
+
+func (s *Schema) HasField(fieldName string) bool {
+	for _, fn := range s.fields {
+		if fn == fieldName {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Schema) Fields() []string {
+	return s.fields
+}
+
+func (s *Schema) addField(fieldName string, fieldType FieldType, length int) {
+	s.fields = append(s.fields, fieldName)
+	s.fieldInfo[fieldName] = NewFieldInfo(fieldType, length)
+}
+
+func (s *Schema) fieldType(fieldName string) (FieldType, error) {
+	fi, ok := s.fieldInfo[fieldName]
+	if !ok {
+		return Unknown, fmt.Errorf("cannot get field info: field name [%s]", fieldName)
+	}
+	return fi.fieldType, nil
+}
+
+func (s *Schema) length(fieldName string) (int, error) {
+	fi, ok := s.fieldInfo[fieldName]
+	if !ok {
+		return 0, fmt.Errorf("cannot get field info: field name [%s]", fieldName)
+	}
+	return fi.length, nil
+}
+
+func (s *Schema) lengthInBytes(fieldName string) (int, error) {
+	fi, ok := s.fieldInfo[fieldName]
+	if !ok {
+		return 0, fmt.Errorf("cannot get field info: field name [%s]", fieldName)
+	}
+
+	switch fi.fieldType {
+	case Integer:
+		return IntByteSize, nil
+	case String:
+		strlen, err := s.length(fieldName)
+		if err != nil {
+			return 0, err
+		}
+		return file.MaxLength(strlen), nil
+	default:
+		return 0, fmt.Errorf("invalid field type [%d]", fi.fieldType)
+	}
+}
