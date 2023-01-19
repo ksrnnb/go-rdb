a6a604c66d8ae82331f1a39ab65b25fb352f0f9f
K. Okabe <48155865+ksrnnb@users.noreply.github.com>
2023-01-20 08:19:39 +0900

    add view manager

---
+++ metadata/view_manager.go
@@ -0,0 +1,96 @@
+package metadata
+
+import (
+	"github.com/ksrnnb/go-rdb/record"
+	"github.com/ksrnnb/go-rdb/tx"
+)
+
+const MaxViewDefinitionLength = 100
+
+const viewCategoryTableName = "view_categories"
+
+const (
+	viewNameField       = "table_name"
+	viewDefinitionField = "slot_size"
+)
+
+type ViewManager struct {
+	tm *TableManager
+}
+
+func NewViewManager(isNew bool, tm *TableManager, tx *tx.Transaction) (*ViewManager, error) {
+	vm := &ViewManager{tm}
+	if isNew {
+		schema := record.NewSchema()
+		schema.AddStringField(viewNameField, MaxFieldNameLength)
+		schema.AddStringField(viewDefinitionField, MaxFieldNameLength)
+		err := vm.tm.CreateTable(viewCategoryTableName, schema, tx)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return vm, nil
+}
+
+func (vm *ViewManager) CreateView(viewName string, definition string, tx *tx.Transaction) error {
+	layout, err := vm.tm.Layout(viewCategoryTableName, tx)
+	if err != nil {
+		return err
+	}
+	ts, err := record.NewTableScan(tx, viewCategoryTableName, layout)
+	if err != nil {
+		return err
+	}
+	err = ts.SetString(viewNameField, viewName)
+	if err != nil {
+		return err
+	}
+	err = ts.SetString(viewDefinitionField, definition)
+	if err != nil {
+		return err
+	}
+	return ts.Close()
+}
+
+func (vm *ViewManager) Definition(viewName string, tx *tx.Transaction) (string, error) {
+	definition := ""
+	layout, err := vm.tm.Layout(viewCategoryTableName, tx)
+	if err != nil {
+		return "", err
+	}
+
+	ts, err := record.NewTableScan(tx, viewCategoryTableName, layout)
+	if err != nil {
+		return "", err
+	}
+
+	hasNext, err := ts.Next()
+	if err != nil {
+		return "", err
+	}
+
+	for hasNext {
+		vn, err := ts.GetString(viewNameField)
+		if err != nil {
+			return "", err
+		}
+		if vn == viewName {
+			definition, err = ts.GetString(viewDefinitionField)
+			if err != nil {
+				return "", err
+			}
+			break
+		}
+		newHasNext, err := ts.Next()
+		if err != nil {
+			return "", err
+		}
+		hasNext = newHasNext
+	}
+
+	err = ts.Close()
+	if err != nil {
+		return "", err
+	}
+	return definition, nil
+}
